21107c5cfe6e95676882cbec19d2d7117383a1dc
Vitor Lima <vitorlimadev@outlook.com>
2021-02-02 18:40:20 -0300

    chore: add ecto monitoring in dashboard

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Exmon.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:ecto_psql_extras, "~> 0.2"},
       {:argon2_elixir, "~> 2.0"}
     ]
   end
