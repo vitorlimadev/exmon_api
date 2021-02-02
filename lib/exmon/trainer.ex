9486a0d6d1ec61ed474b055c7901813ccdeec7e7
Vitor Lima <vitorlimadev@outlook.com>
2021-02-02 12:53:33 -0300

    feat: hash trainer password

--- lib/exmon/trainer.ex
+++ lib/exmon/trainer.ex
@@ -1,19 +1,30 @@
 defmodule Exmon.Trainer do
+  alias Exmon.Repo
   use Ecto.Schema
   import Ecto.Changeset
 
   @primary_key {:id, Ecto.UUID, autogenerate: true}
 
-  schema "Trainers" do
+  schema "trainers" do
     field :name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
     timestamps()
   end
 
-  @required_params [:name, :password_hash]
+  @required_params [:name, :password]
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
   end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+    |> Repo.insert()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: false} = changeset), do: changeset
 end
